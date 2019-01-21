06f981845ec28a79b7a53e784b5458d29efa682c
kyleconyers <conyersky@gmail.com>
2019-01-21 14:09:54 -0800

    first commit

---
+++ build/precache-manifest.910cd5d0905f01e98a13209280f30483.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "652e1d5bd2e63ca14777",
+    "url": "/clicky-game/static/js/main.652e1d5b.chunk.js"
+  },
+  {
+    "revision": "441b227c7c9e041cd9b4",
+    "url": "/clicky-game/static/js/1.441b227c.chunk.js"
+  },
+  {
+    "revision": "652e1d5bd2e63ca14777",
+    "url": "/clicky-game/static/css/main.6ba5ef33.chunk.css"
+  },
+  {
+    "revision": "d0c6c6f2dffead3f10eaa9f3867cad10",
+    "url": "/clicky-game/index.html"
+  }
+];
